ba33ad4c8dbe58e3250ec0341d131a843545d3ff
Shalim <45618716+smn726@users.noreply.github.com>
2018-12-17 16:45:23 -0700

    Add files via upload

---
+++ DRILL_Exercises_in_Probability.py
@@ -0,0 +1,42 @@
+
+# coding: utf-8
+
+# In[4]:
+
+
+# (1)
+# Possible outcomes of patterns: 2^4 = 16
+# Each pattern has a 1/16 chance of occuring.
+
+# To check, we list outcomes:
+# HHHH, HHHT, HHTT, HTTT, HHTH, HTHH, HTTH, HTHT, TTTT, TTTH, TTHH, THHH, TTHT, THTT, THHT, THTH
+# Each pattern in question is listed once and given equal probability of the balance coin flip, each pattern has also equal
+# chance.
+
+
+# In[5]:
+
+
+# (2)
+# 24 women | 21 men : 45 Total
+# Not choosing a man = Choosing a woman = 24/45
+
+
+# In[6]:
+
+
+# (3)
+# P(Bernice travels by plane this year) = 10% = 0.10
+# P(Plane crash) = 0.005% = 0.00005
+# P(Bernice will be in a plane crash) = 0.10 * 0.00005 =  0.000005 = 0.0005%
+
+
+# In[7]:
+
+
+# (4)
+# One can assume that the longer someone spends on the website, the higher chance they have to be 
+# asked to complete the short survey. Therefore, the sample population mainly consists of those users
+# that spent a longer amount of time on the website and is not necessarily represenatative of those
+# users that spent less time on the website.
+
