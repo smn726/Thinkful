b63f4b59004beb6fc7c4f4f58f5afb0db1cd1702
Shalim <45618716+smn726@users.noreply.github.com>
2018-12-18 18:13:28 -0700

    Add files via upload

---
+++ Evaluating_Data_Sources.py
@@ -0,0 +1,41 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+# Data Source: Amsterdam availability data scraped from AirBnB on December 24th. 
+# Question: What are the popular neighborhoods in Amsterdam?
+
+# The scope of the data is so narrow you wouldn't be able to tell which neighborhoods are popular since
+# people travel very differently on major holidays. We could assume there will be no availability
+# in any neighborhoods, and so the data might sugest all neighborhoods are equally popular.
+
+# An alternative might be to analyze AirBnB price data in Amsterdam on December 24th. Higher price AirBnb rentals
+# might indicate more popular areas in Amsterdam.
+
+
+# In[2]:
+
+
+# Data Source: Mental health services use on September 12, 2001 in San Francisco, CA and New York City, NY. 
+# Question: How do patterns of mental health service use vary between cities?
+
+# The data will grealty vary due to the terroristic attack in NY on 9/11/01 inflicting a great deal of mental health
+# trauma to a great number of New Yorkers. 
+
+# An alternative question you could ask is "How do patterns of mental health service use vary between a city
+# recently affected by a terrost attack and a city that has not?"
+
+
+# In[3]:
+
+
+# Data Source: Armenian Pub Survey. 
+# Question: What are the most common reasons Armenians visit local pubs?
+
+# The data comes from mainly students, aged less than 24 and from the capital. It is too narrow in that it does
+# represent all of Armenian residents throughout the country.
+
+# You could ask, "What are the most common reasons Armenian students in the capital region visit local pubs?"
+
