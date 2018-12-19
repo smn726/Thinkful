93c41f0db8b583ee543b8544bd7f26719dc215b0
Shalim <45618716+smn726@users.noreply.github.com>
2018-12-18 19:46:09 -0700

    Add files via upload

---
+++ Challenge_Beware_of_Monty_Hall.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# In[3]:
+
+
+# I was already aware and familiar with the Monty Hall Problem but I will discuss my original train
+# of thought before I learned about the solution years ago.
+
+# I first assumed the likelihood of each door having the grand prize to
+# be equal or 1/3 for each door. And even after choosing a door, and being shown a dud door,
+# the probability of my second choice would be a 50/50 shot, so I chose to keep my original choice.
+
+# However, I have never been exposed to the Baye's Theorem approach to it, only a counting method that ultimately
+# suggested that you had a better likelihood when switching than keeping your original choice.
+
